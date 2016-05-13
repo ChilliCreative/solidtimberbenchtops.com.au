487d8ab2d8ed1c1e15bf0919e11f2b3b0d9560fe
Dale Grant <dale@thetallone.co.uk>
2016-05-13 12:56:30 +1000

    Turned on WP_DEBUG for staging env

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -1,7 +1,9 @@
 <?php
 /** Staging */
 ini_set('display_errors', 0);
+define('WP_DEBUG', true);
 define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
