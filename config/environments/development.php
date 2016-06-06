48e2d055f2de0e182740fdb4df536f1dd5b56e94
Dale Grant <dale@thetallone.co.uk>
2016-06-06 23:22:20 +1000

    Fix Contact form 7 alignment issue.

--- config/environments/development.php
+++ config/environments/development.php
@@ -4,3 +4,5 @@ define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
 define('SCRIPT_DEBUG', true);
+/** Contact form 7 */
+define( 'WPCF7_AUTOP', false );
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -7,3 +7,5 @@ define('WP_DEBUG_LOG', true);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+/** Contact form 7 */
+define( 'WPCF7_AUTOP', false );
